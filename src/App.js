a85e622b81d83d87fbca59364b5ae93d22d594cb
Justus <justus.onyancha@moringaschool.com>
2023-01-20 12:32:28 +0300

    initialize repo on using a useRef and timer using setIntervals and clearIntervals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { useRef, useState } from 'react';
+
+
+
+export default function App(){
+
+  const [ timepiece, setTimePiece ] = useState(0);
+
+  const timerID = useRef(null)
+
+  const startTimer = () => {
+    timerID.current = setInterval(() =>{
+      setTimePiece(currSecs => currSecs +1)
+    }, 1000);
+  }
+
+
+   const stopTimer = () =>{
+    clearInterval(timerID.current);
+   }
+  
+
+
+
+  return(
+    <>
+    <div>
+      <button onClick={startTimer} >Start</button>
+
+
+      <button onClick={stopTimer} >Stop</button>
+      <p>{timepiece}</p>
     </div>
+    </>
   );
-}
-
-export default App;
+} 
